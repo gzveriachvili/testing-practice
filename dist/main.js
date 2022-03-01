0e21470a886bd708468d69b6c44b6d45856a8d25
gzver <gzveriachvili@gmail.com>
2022-03-01 03:20:31 +0200

    Skeleteon has been created. No tests written yet.

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('hi');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxLQUFLLENBQUMsSUFBRCxDQUFMLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcmFjdGljZS1wcm9qZWN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KCdoaScpO1xyXG4iXSwibmFtZXMiOlsiYWxlcnQiXSwic291cmNlUm9vdCI6IiJ9
