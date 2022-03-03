a836b4d8886a62300946980ba6c27eaf23a915c5
gzver <gzveriachvili@gmail.com>
2022-03-03 02:04:37 +0200

    Caesar cipher bugfix

--- src/modules/code/code/caesar_cipher.js
+++ src/modules/code/code/caesar_cipher.js
@@ -6,19 +6,42 @@ const cipher = (string) => {
   string = string.toLowerCase();
   string = string.split('');
 
-  for (let i = 0; i <= plain.length - 1; i++) {
-    if (string.includes(plain[i])) {
-      if (plain[i] === 'z') {
-        plain[i] = 'a';
-        arr.push(plain[i]);
-      } else {
-        arr.push(plain[i + 1]);
+  function getCount() {
+    let count = 1;
+    for (let i = 0; i <= string.length - 1; i++) {
+      if (string[i] === string[i + 1]) {
+        count++;
       }
     }
-    if (string.includes(punctuation[i])) {
-      arr.push(punctuation[i]);
+    return count;
+  }
+
+  function doLoop(n) {
+    for (let i = 0; i <= plain.length - 1; i++) {
+      if (string.includes(plain[i])) {
+        if (string.includes(punctuation[i])) {
+          arr.push(punctuation[i]);
+        }
+
+        if (plain[i] === 'z') {
+          plain[i] = 'a';
+          for (let x = 0; x < 2; x++) {
+            arr.push(plain[i]);
+          }
+        } else {
+          if (string[i] == string[i + 1]) {
+            for (let x = 0; x < n; x++) {
+              arr.push(plain[i + 1]);
+            }
+          } else {
+            arr.push(plain[i + 1]);
+          }
+        }
+      }
     }
   }
+
+  doLoop(getCount());
 
   return arr.join('');
 };
--- src/modules/code/test/caesar_cipher.test.js
+++ src/modules/code/test/caesar_cipher.test.js
@@ -1,9 +1,13 @@
 const cipher = require('../code/caesar_cipher');
 
 test('replace letters with a shift of 1', () => {
-  expect(cipher('a.bc')).toMatch('bc.d');
+  expect(cipher('abc')).toMatch('bcd');
 });
 
 test('replace letters with a shift of 1', () => {
-  expect(cipher('aBcDe')).toMatch('bcdef');
+  expect(cipher('aaa')).toMatch('bbb');
+});
+
+test('replace letters with a shift of 1', () => {
+  expect(cipher('aA.Bc')).toMatch('bb.cd');
 });
